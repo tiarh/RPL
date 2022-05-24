e1cfbd0c379d4addb69ef5a1a50a679a84ec6208
alkhawarizmi <alkhawarizmiutm@gmail.com>
2022-05-24 10:53:46 +0700

    new project

---
+++ tokopekita/dbconnect.php
@@ -0,0 +1,9 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","root","tanpasandi","tokopekita");
+
+if(!$conn){
+	echo "gagal konek database menn";
+} else {
+	
+};
